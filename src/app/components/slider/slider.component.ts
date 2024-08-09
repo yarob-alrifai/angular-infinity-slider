f65270193525a0d8755f1e2916a0565dba37d469
yarob <yalrifai41@gmail.com>
2024-08-09 13:36:53 +0300

    after the interview

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -2,12 +2,13 @@ import {
   Component,
   OnInit,
   Renderer2,
-  ViewChild,
   ElementRef,
   signal,
+  viewChild,
 } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+
 interface Item {
   key: number;
 }
@@ -23,28 +24,31 @@ export class SliderComponent implements OnInit {
   // green =====================================def=====================================
   // orange we can change the next as we want
 
+
+
   transformValue = signal('translateX(0)');
-  itemsNumber = 50; //Items number
-  visibleItemsCount = 5; // Number of visible items on the screen, we can change it as we want
-  winningBlock = 14; //Winning block
+  itemsNumber = signal(50); //Items number
+  visibleItemsCount = signal(5); // Number of visible items on the screen, we can change it as we want
+  winningBlock = signal(14); //Winning block
   // orange we can change the prev as we want
   // blue====our data
   ourDate = signal<Item[]>(
-    Array.from({ length: this.itemsNumber }, (_, i) => ({ key: i + 1 }))
+    Array.from({ length: this.itemsNumber() }, (_, i) => ({ key: i + 1 }))
   ); //Gerate our items
   items = signal<Item[]>([]); //To display on the screen
-  helper = this.ourDate(); //For reset the items when stop
   data = [...this.ourDate()]; //We will use it to add items every time to our items array
+  helper = this.ourDate(); //For reset the items when stop
   // blue====our data
-  currentIndex = signal<number>(Math.floor(this.visibleItemsCount / 2)); //Started index
+  currentIndex = signal<number>(Math.floor(this.visibleItemsCount() / 2)); //Started index
   isRunning = signal<boolean>(false);
   selectedItemIndex = signal(-1); //Random slected item to stop on it
   isWinner = signal<boolean>(false); //Use it in the dialog
   dialogTitle = signal<string>(''); //Dialog title we will change it then
   showDialog = signal<boolean>(false); //To show dialog and show message when stop
 enableButton = signal(true)
-  @ViewChild('dialog') dialog!: ElementRef<HTMLDialogElement>; //Get to the dialog
-  @ViewChild('carouselList', { static: true }) carouselList!: ElementRef; //Get to the carousel list
+
+dialog = viewChild<ElementRef>("dialog");//Get to the dialog
+carouselList = viewChild<ElementRef>("carouselList");//Get to the carousel list
 
   // fire =====================================def=====================================
 
@@ -74,24 +78,31 @@ enableButton = signal(true)
   // green =====================================initCarousel=====================================initCarousel
 
   initCarousel() {
-    this.items.set(this.ourDate().slice(0, this.visibleItemsCount)); //Upload 5 items in the items array
-    this.data.splice(0, this.visibleItemsCount); //Delete first 5 items from data array so we can shift first item add push it in items array, to avoid repetation
+    this.items.set(this.ourDate().slice(0, this.visibleItemsCount())); //Upload 5 items in the items array
+    this.data.splice(0, this.visibleItemsCount()); //Delete first 5 items from data array so we can shift first item add push it in items array, to avoid repetation
 
     this.applyStyles(); //To edit the style because we added new items
     this.rotateCarousel(); //Rotate the carousel after add item
   }
 
+  // green =====================================applyStyle=====================================applyStyle
+
+   applyStyle ()  {
+    this.renderer.setStyle(this.carouselList()?.nativeElement, 'transition', '');
+  };
+
+
   // green =====================================applyStyles=====================================applyStyles
 
   applyStyles() {
     const cells = Array.from(
-      this.carouselList.nativeElement.querySelectorAll('.carousel__cell')
+      this.carouselList()?.nativeElement.querySelectorAll('.carousel__cell')
     ) as HTMLElement[];
     cells.forEach((cell: HTMLElement) => {
       this.renderer.setStyle(
         cell,
         'flex',
-        `0 0 ${100 / this.visibleItemsCount}%`
+        `0 0 ${100 / this.visibleItemsCount()}%`
       );
     });
   }
@@ -99,11 +110,11 @@ enableButton = signal(true)
 
   rotateCarousel() {
     const transformValue = `translateX(${
-      (-(this.currentIndex() - Math.floor(this.visibleItemsCount / 2)) * 100) /
-      this.visibleItemsCount
+      (-(this.currentIndex() - Math.floor(this.visibleItemsCount() / 2)) * 100) /
+      this.visibleItemsCount()
     }%)`; //The movment will be to the left side with animation, then the selected item will be at the center
     this.renderer.setStyle(
-      this.carouselList.nativeElement,
+      this.carouselList()?.nativeElement,
       'transform',
       transformValue
     );
@@ -114,25 +125,30 @@ enableButton = signal(true)
   _rotateCarousel() {
     // Compute the transform value
     const transformValue = `translateX(${
-      (-(this.currentIndex() - Math.floor(this.visibleItemsCount / 2)) * 100) /
-      this.visibleItemsCount
+      (-(this.currentIndex() - Math.floor(this.visibleItemsCount() / 2)) * 100) /
+      this.visibleItemsCount()
     }%)`;
     // Temporarily disable transitions
     this.renderer.setStyle(
-      this.carouselList.nativeElement,
+      this.carouselList()?.nativeElement,
       'transition',
       'none'
     );
     this.renderer.setStyle(
-      this.carouselList.nativeElement,
+      this.carouselList()?.nativeElement,
       'transform',
       transformValue
     );
 
     // Restore transitions after a short delay
-    setTimeout(() => {
-      this.renderer.setStyle(this.carouselList.nativeElement, 'transition', '');
-    }, 20);
+
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        this.renderer.setStyle(this.carouselList()?.nativeElement, 'transition', '');
+      });
+    });
+
+
   }
 
   // green =====================================startSlider=====================================startSlider
@@ -151,7 +167,7 @@ enableButton = signal(true)
   // green =====================================getRandomIndex=====================================getRandomIndex
 
   getRandomIndex(): number {
-    return Math.floor(Math.random() * 10) + this.winningBlock; //Get random index depending on winning block that we selected
+    return Math.floor(Math.random() * 10) + this.winningBlock(); //Get random index depending on winning block that we selected
   }
 
   // green =====================================performFullRotation=====================================performFullRotation
@@ -203,25 +219,34 @@ enableButton = signal(true)
         clearInterval(alignmentInterval);
         this.isRunning.set(false);
 
-        setTimeout(() => {
-          //Here we will wait for 500ms then the dialog will appear, to display the result
-          if(this.selectedItemIndex()===14){          this.isWinner.set(true);
-          }
-          this.dialogTitle.set(this.isWinner() ? 'Congratulations!' : 'Oops!');
-          this.showDialog.set(true);
-          this.openDialog();
-          this.enableButton.set(true)
-          // fire Reset items array. Put in items array just 50 items and delete the old value
-          this.items.set(
-            this.helper.slice(
-              0,
-              this.selectedItemIndex() + Math.round(this.visibleItemsCount / 2)
-            )
-          );
-          this.currentIndex.update((index) => index % this.itemsNumber);
-          // fire After reset the items array we will move to the index %items.length, with out animation
-          this._rotateCarousel();
-        }, 500);
+
+
+
+        requestAnimationFrame(() => {
+          requestAnimationFrame(() => {
+           //Here we will wait for 500ms then the dialog will appear, to display the result
+           if(this.selectedItemIndex()===14){          this.isWinner.set(true);
+           }
+           this.dialogTitle.set(this.isWinner() ? 'Congratulations!' : 'Oops!');
+           this.showDialog.set(true);
+           this.openDialog();
+           this.enableButton.set(true)
+           // fire Reset items array. Put in items array just 50 items and delete the old value
+           this.items.set(
+             this.helper.slice(
+               0,
+               this.selectedItemIndex() + Math.round(this.visibleItemsCount() / 2)
+             )
+           );
+           this.currentIndex.update((index) => index % this.itemsNumber());
+           // fire After reset the items array we will move to the index %items.length, with out animation
+           this._rotateCarousel();
+          });
+        });
+
+
+
+
       } else {
         this.add();
       }
@@ -229,19 +254,19 @@ enableButton = signal(true)
   }
   // green =====================================calculateOffset=====================================calculateOffset
 
-  calculateOffset(): number {
+  calculateOffset(): number {//вычисления смещения
     let theRealIndexForSelected = -1;
     // Calculate the offset depending on our current index
     theRealIndexForSelected =
-      Math.floor(this.currentIndex() / this.itemsNumber) * this.itemsNumber +
+      Math.floor(this.currentIndex() / this.itemsNumber()) * this.itemsNumber() +
       this.selectedItemIndex();
 
     console.log('selected Item Index : ' + this.selectedItemIndex());
     // console.log('currentIndex' + this.currentIndex());
     if (theRealIndexForSelected < this.currentIndex()) {
       theRealIndexForSelected =
-        Math.floor(this.currentIndex() / this.itemsNumber) *
-          (this.itemsNumber * 2) +
+        Math.floor(this.currentIndex() / this.itemsNumber()) *
+          (this.itemsNumber() * 2) +
         this.selectedItemIndex();
     }
     return theRealIndexForSelected;
@@ -251,12 +276,12 @@ enableButton = signal(true)
   // orange this function to now if the item is in the center of the view(and the selected index right now) to applay some css on it
   isCenterItem(index: number): boolean {
     const centerIndex = Math.floor(
-      this.visibleItemsCount / Math.floor(this.visibleItemsCount / 2)
+      this.visibleItemsCount() / Math.floor(this.visibleItemsCount() / 2)
     );
 
     let currentCenterIndex = 0;
 
-    if (this.visibleItemsCount < 5) {
+    if (this.visibleItemsCount() < 5) {
       currentCenterIndex =
         (this.currentIndex() - 3 + centerIndex) % this.helper.length;
     } else {
@@ -272,12 +297,11 @@ enableButton = signal(true)
 
   closeDialog() {
     this.showDialog.set(false);
-    const dialog = this.dialog.nativeElement;
+    const dialog = this.dialog()?.nativeElement;
     dialog.close();
   }
-
   openDialog() {
-    const dialog = this.dialog.nativeElement;
+    const dialog = this.dialog()?.nativeElement;
     dialog.showModal();
 
   }
